bfff833288953e44f49b74a3750e1f5438716160
Trungpham2k4 <trungpdb2k4@gmail.com>
2026-05-31 21:31:30 +0700

    add 1 more edge case

--- src/main/java/fa/training/service/LineItemService.java
+++ src/main/java/fa/training/service/LineItemService.java
@@ -1,20 +1,34 @@
 package fa.training.service;
 
 import fa.training.dao.LineItemDAO;
+import fa.training.dao.OrderDAO;
+import fa.training.dao.ProductDAO;
 import fa.training.dao.impl.LineItemDAOImpl;
+import fa.training.dao.impl.OrderDAOImpl;
+import fa.training.dao.impl.ProductDAOImpl;
 import fa.training.entities.LineItem;
 
 import java.util.List;
 
 public class LineItemService {
     private final LineItemDAO lineItemDAO;
+    private final OrderDAO orderDAO;
+    private final ProductDAO productDAO;
     public LineItemService(){
         lineItemDAO = new LineItemDAOImpl();
+        orderDAO = new OrderDAOImpl();
+        productDAO = new ProductDAOImpl();
     }
     public List<LineItem> getAllItemsByOrderId(int orderId) {
         return lineItemDAO.findByOrderId(orderId);
     }
     public boolean addLineItem(LineItem item) {
+        if(orderDAO.findById(item.getOrderId()) == null) {
+            return false;
+        }
+        if(productDAO.findById(item.getProductId()) == null) {
+            return false;
+        }
         return lineItemDAO.save(item);
     }
 }
